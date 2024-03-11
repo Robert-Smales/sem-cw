c0b85d023fb04fce0cb472adc3ffb57dc77d6c8d
harris spratt <harrisspratt5@gmail.com>
2024-03-11 16:36:33 +0000

    added app connection and also population report

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,88 @@
 package com.napier.sem;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Boo yah!");
+
+import java.sql.*;
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class App {
+    private Connection con = null;
+
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
+                Thread.sleep(30000);
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException | InterruptedException sqle) {
+                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(sqle.getMessage());
+            }
+        }
+    }
+
+    public void disconnect() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    public List<Country> getAllCountriesByPopulation() {
+        List<Country> countries = new ArrayList<>();
+        try {
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "SELECT * FROM country " +
+                            "ORDER BY Population DESC";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()) {
+                Country country = new Country(
+                        rset.getString("Code"),
+                        rset.getString("Name"),
+                        rset.getString("Continent"),
+                        rset.getString("Region"),
+                        rset.getDouble("SurfaceArea"),
+                        rset.getInt("IndepYear"),
+                        rset.getInt("Population"),
+                        rset.getDouble("LifeExpectancy"),
+                        rset.getDouble("GNP"),
+                        rset.getDouble("GNPOld"),
+                        rset.getString("LocalName"),
+                        rset.getString("GovernmentForm"),
+                        rset.getString("HeadOfState"),
+                        rset.getInt("Capital"),
+                        rset.getString("Code2")
+                );
+                countries.add(country);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries by population");
+        }
+        return countries;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.connect();
+        List<Country> countries = app.getAllCountriesByPopulation();
+        for (Country country : countries) {
+            System.out.println(country.name + " - Population: " + country.population);
+        }
+        app.disconnect();
     }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,42 @@
+package com.napier.sem;
+
+public class Country {
+    public String code;
+    public String name;
+    public String continent;
+    public String region;
+    public double surfaceArea;
+    public int indepYear;
+    public int population;
+    public double lifeExpectancy;
+    public double gnp;
+    public double gnpOld;
+    public String localName;
+    public String governmentForm;
+    public String headOfState;
+    public int capital;
+    public String code2;
+    // Add more fields as needed
+
+    // Constructor
+    public Country(String code, String name, String continent, String region, double surfaceArea, int indepYear, int population, double lifeExpectancy, double gnp, double gnpOld, String localName, String governmentForm, String headOfState, int capital, String code2) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.surfaceArea = surfaceArea;
+        this.indepYear = indepYear;
+        this.population = population;
+        this.lifeExpectancy = lifeExpectancy;
+        this.gnp = gnp;
+        this.gnpOld = gnpOld;
+        this.localName = localName;
+        this.governmentForm = governmentForm;
+        this.headOfState = headOfState;
+        this.capital = capital;
+        this.code2 = code2;
+    }
+
+    // Getter and setter methods can be added as needed
+}
+
